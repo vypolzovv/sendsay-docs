8f5c79574aed7a76a76c52bd795201c2b0e3cacc
aleksnick <aleksnick01@gmail.com>
2022-05-30 18:49:07 +0300

    fix(sendsay-docs): fix travis deploy (#18)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export BUILD_PATH=build-docs
-export RELEASE_NAME=$(git describe --tags --abbrev=0);
+export RELEASE_NAME=$(git describe --all);
 export MESSAGE=sendsay-docs\ $BUILD_PATH\ $RELEASE_NAME
 
 git clone git@github.com:sendsay-ru/sendsay-frontend-builds.git /tmp/sendsay-frontend-builds
@@ -10,7 +10,7 @@ yarn run build
 
 rm -rf /tmp/sendsay-frontend-builds/$BUILD_PATH
 
-yes | cp -R build/* /tmp/sendsay-frontend-builds/$BUILD_PATH
+yes | cp -R build /tmp/sendsay-frontend-builds/$BUILD_PATH
 
 cd /tmp/sendsay-frontend-builds
 
