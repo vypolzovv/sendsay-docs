f2ad39a281def52090ab0224e5ab01dc2e9d357c
Aleksey Nikolaev <nikolaev@iprojects.ru>
2023-12-20 14:05:38 +0300

    ci(sendsay-docs): build project in another repo

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
 export BUILD_PATH=build-docs
+export BRANCH=stable
 export RELEASE_NAME=$(git log -1 --pretty=format:"%s %h %an")
 export MESSAGE=$RELEASE_NAME
+export BUILD_REPO=sendsay-docs-build
 
-git clone git@github.com:sendsay-ru/sendsay-frontend-builds.git /tmp/sendsay-frontend-builds
+git clone git@github.com:sendsay-ru/$BUILD_REPO.git /tmp/$BUILD_REPO
 
 yarn run build
 
-rm -rf /tmp/sendsay-frontend-builds/$BUILD_PATH
+rm -rf /tmp/$BUILD_REPO/$BUILD_PATH
 
-yes | cp -R build /tmp/sendsay-frontend-builds/$BUILD_PATH
+yes | cp -R build /tmp/$BUILD_REPO/$BUILD_PATH
 
-cd /tmp/sendsay-frontend-builds
+cd /tmp/$BUILD_REPO
 
 echo $MESSAGE > $BUILD_PATH/COMMIT
 
 git add .
 git commit -m "$MESSAGE"
-git push origin master
+git push origin $BRANCH
